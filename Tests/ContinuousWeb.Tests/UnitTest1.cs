6dea5745bcb693b202ee078881cc0bcf6cb78729
jtownsley@cardinalsolutions.com <jtownsley@cardinalsolutions.com>
2018-03-05 12:13:22 -0500

    Added tests

---
+++ Tests/ContinuousWeb.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using continuousweb.Controllers;
+namespace continuousweb.tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {        
+        [TestMethod]
+        public void AboutPageTest()
+        {
+            var homeController = new HomeController();
+            var result = homeController.About();
+        }
+    }
+}
